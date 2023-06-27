9d4dd634528bea8cdbce0337b1959b24a68e154e
DangerDi <130923954+DangerDi@users.noreply.github.com>
2023-06-27 17:12:17 +0300

    Add files via upload

---
+++ mission6.py
@@ -0,0 +1,16 @@
+k=int(input())
+arr=['a','b','c','d','e']
+res=''
+for i in range(2**k):
+  x=input().split()
+  if x[k]=='0':
+      res+='('
+      for j in range(len(x)-1):
+        if j<k and j>0:
+          res+=' + '
+        if x[j]=='0':
+          res+=arr[j]
+        else:
+          res+='-'+arr[j]
+      res+=')'
+print(res)
